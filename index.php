61f9855fc44b7f59475d3bd574630775ece155bf
Sameeranimal <147604643+Sameeranimal@users.noreply.github.com>
2024-11-30 20:34:57 +0100

    update

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION["user_id"])) {
     <title>Home</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <link rel="stylesheet" href="index.css">
     
@@ -37,7 +37,10 @@ if (isset($_SESSION["user_id"])) {
     
     <img src="morphens.png" width="500px" alt="">
     
-    
+    <a href="https://www.linkedin.com/company/morphe/posts/?feedView=all" class="fa fa-linkedin"></a>
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-youtube"></a>
+
     
     <?php if (isset($user)): ?>
         
